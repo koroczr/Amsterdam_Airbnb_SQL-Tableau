039492d825a0a89ec496cf8981a0e1173639272c
Richárd Köröcz <71522609+koroczr@users.noreply.github.com>
2020-10-06 16:52:13 +0200

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -4,6 +4,8 @@ from
 listings;
 
 
+# What type of ’minimum nights’ were prefered based on the number of the reviews?
+
 SELECT 
     l.minimum_nights, COUNT(r.date) AS number_dates
 FROM
@@ -14,7 +16,7 @@ GROUP BY l.minimum_nights
 ORDER BY COUNT(r.date) DESC
 LIMIT 10;
 
-
+# What kind of ’room type’ was the most visited one?
 SELECT 
     l.room_type, COUNT(r.date) AS number_dates
 FROM
@@ -25,6 +27,7 @@ GROUP BY l.room_type
 ORDER BY COUNT(r.date) DESC
 LIMIT 10;
 
+# Avarage price based on the neighborhoods (with and without number of the minimum nights).
 
 SELECT 
     neighbourhood, avg(price)*avg(minimum_nights) AS price_w_nights
@@ -34,10 +37,6 @@ GROUP BY neighbourhood
 ORDER BY avg(price)*avg(minimum_nights) DESC
 LIMIT 1000;
 
-
-
-
-
 SELECT 
     neighbourhood, AVG(minimum_nights) AS avarage_minimum_nights
 FROM
@@ -45,3 +44,5 @@ FROM
 GROUP BY neighbourhood
 ORDER BY AVG(minimum_nights) DESC
 LIMIT 1000;
+
+
